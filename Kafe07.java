6fd5f3c374a90afcdd76e7f0038f705db5a855af
Geveriell <erzaardhyaputra@gmail.com>
2025-11-26 08:10:58 +0700

    percobaan 1

---
+++ Kafe07.java
@@ -0,0 +1,16 @@
+public class Kafe07 {
+
+    // fungsi tanpa paramenterr
+    public static void Menu() {
+        System.out.println("=== MENU KAFE ===");
+        System.out.println("1. Kopi        - Rp 10.000");
+        System.out.println("2. Teh         - Rp 8.000");
+        System.out.println("3. Roti        - Rp 12.000");
+        System.out.println("4. Nasi Goreng - Rp 20.000");
+        System.out.println("=====================");
+    }
+
+    public static void main(String[] args) {
+        Menu(); // memanggil fungsi Menu
+    }
+}
